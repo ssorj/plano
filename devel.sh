90597a767732c23cfb413f1f48e420b16d874734
Justin Ross <jross@redhat.com>
2015-02-27 08:55:52 -0500

    Add a devel.sh

---
+++ devel.sh
@@ -0,0 +1,17 @@
+path=$(python - <<EOF
+from __future__ import print_function
+
+import distutils.sysconfig as config
+import os
+
+install_dir = os.path.abspath("install")
+plat_path = config.get_python_lib(plat_specific=True, prefix=install_dir)
+path = config.get_python_lib(prefix=install_dir)
+
+print("{}:{}".format(plat_path, path))
+EOF
+)
+
+PYTHONPATH=$path:$PYTHONPATH
+
+export PYTHONPATH
