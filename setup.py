af67f507fcef54c6d9db259de92c17d0e1dee618
Justin Ross <jross@redhat.com>
2015-02-27 09:00:34 -0500

    Correct the version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 from distutils.core import setup
 
 setup(name="plano",
-      version="0.3",
+      version="0.2",
       url="http://www.ssorj.net/projects/plano.html",
       author="Justin Ross",
       author_email="justin.ross@gmail.com",
