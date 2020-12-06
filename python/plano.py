1c712b008bbe893e9adbf6a7eb2bf8edda378d5f
Justin Ross <jross@apache.org>
2020-12-06 09:05:24 -0500

    Support bright colors as well

--- python/plano.py
+++ python/plano.py
@@ -182,20 +182,27 @@ def flush():
     STDOUT.flush()
     STDERR.flush()
 
-_console_colors = {
-    "black": "\u001b[30m",
-    "red": "\u001b[31m",
-    "green": "\u001b[32m",
-    "yellow": "\u001b[33m",
-    "blue": "\u001b[34m",
-    "magenta": "\u001b[35m",
-    "cyan": "\u001b[36m",
-    "white": "\u001b[37m",
+_color_codes = {
+    "black": "\u001b[30",
+    "red": "\u001b[31",
+    "green": "\u001b[32",
+    "yellow": "\u001b[33",
+    "blue": "\u001b[34",
+    "magenta": "\u001b[35",
+    "cyan": "\u001b[36",
+    "white": "\u001b[37",
 }
 
 class console_color(object):
-    def __init__(self, color, file=STDOUT):
-        self.color = _console_colors[color]
+    def __init__(self, color, bright=False, file=STDOUT):
+        elems = [_color_codes[color]]
+
+        if bright:
+            elems.append(";1")
+
+        elems.append("m")
+
+        self.color = "".join(elems)
         self.file = file
 
         self.has_colors = hasattr(self.file, "isatty") and self.file.isatty()
